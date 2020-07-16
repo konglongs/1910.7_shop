e413d600584889f2a870595fb907f58683a01768
kwl <918903531@qq.com>
2020-07-16 10:55:43 +0000

    修改页面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel_{{env('APP_ENV')}}
                 </div>
 
                 <div class="links">
